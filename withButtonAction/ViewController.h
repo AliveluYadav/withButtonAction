e1fcb0dbcc11e0c2112ba9a54350e943fec34856
Alivelu Ravula <alivelu7193@achiever-103.local>
2018-02-03 10:00:18 +0530

    Initial Commit

---
+++ withButtonAction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  withButtonAction
+//
+//  Created by Alivelu Ravula on 2/3/18.
+//  Copyright © 2018 Alivelu Ravula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ withButtonAction/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  withButtonAction
+//
+//  Created by Alivelu Ravula on 2/3/18.
+//  Copyright © 2018 Alivelu Ravula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
